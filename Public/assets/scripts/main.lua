7e2b33d4a1e49b256fdfe185fda4a25e4f8bb2d6
YueKun <mail.yuekun@gmail.com>
2021-06-04 09:22:11 +0800

    111

--- Public/assets/scripts/main.lua
+++ Public/assets/scripts/main.lua
@@ -1,44 +1,39 @@
-require("BoxLib");
+ require("BoxLib");
 -- 屏幕常量
 device.keepWake();
 currentIndex = 1;
+count = 0;
 
 -- 查找广告按钮并点击
 function findAdButtonAndClick()
-    local wid= widget.desc("观看创意广告");
-    if wid ~= nil then
-        mSleep(1000)
-        toast("控件已找到")
-        mSleep(1000)
-        --点击控件
-        widget.click(wid)
-        mSleep(1000)
-        --点击控件
-        widget.click(wid)
-        mSleep(3000)
-        return true;
-    else
-        mSleep(1000)
-        mSleep(1000)
-        return false;
+    for i=1,3,1 do
+        local titles = {"观看创意广告", "Watch creative ads"};
+        for i = 1, #titles do
+            local title = titles[i];
+            local wid= widget.desc(title);
+            if wid ~= nil then
+                mSleep(1000)
+                toast("控件已找到")
+                mSleep(1000)
+                --点击控件
+                widget.click(wid)
+                mSleep(1000)
+                --点击控件
+                widget.click(wid)
+                mSleep(3000)
+                return true;
+            else
+                mSleep(3000)
+            end
+        end
     end
-end
-
--- 返回app
-function backApp()
-    mSleep(getRndNum(3000,5000));
-    local flag = app.runApp("com.yk.sports");
-    if  flag == true then
-        mSleep(5000);
-        -- 查找关闭按钮并点击
-        findCloseButtonAndClick();
-    end
+    return false;
 end
 
 -- 点击
 function tapAction()
-    local num = getRndNum(1,5);
-    if num == 3 then
+    local num = getRndNum(1,10);
+    if num == 6 then
         findOpenButtonAndClick()
     else 
         findCloseButtonAndClick()
@@ -97,24 +92,36 @@ function findCloseButtonAndClick()
     if result == false then
         keycode.back();
         mSleep(3000)
-        local page = getCurrentPage();
-        if page ~= "运动" then
-            local w,h = getScreenSize();
-            event.tap(w - 70, 160);
-        end
     end
 end
 
 function getCurrentPage()
-    local wid= widget.desc("运动");
-    if wid ~= nil then
-        return "运动";
+    local titles = {"运动", "Sports"};
+    for i = 1, #titles do
+        local title = titles[i];
+        local wid = widget.desc(title);
+        if wid ~= nil then
+            return "运动";
+        end
     end
     return nil;
 end
 
 function currentPackage()
-    local apps = {"com.yk.sports", "com.yk.health", "com.yk.sports.vungle"};
+    local apps = {
+        "com.yk.sports", 
+        "com.yk.health",
+        "com.yk.run", 
+        "com.yk.exercise", 
+        "com.yk.climb",
+        "com.yk.walk", 
+        "com.yk.sports.vungle",  
+        "com.yk.health.plus",  
+        "com.yk.run.plus",  
+        "com.yk.exercise.plus",
+        "com.yk.climb.plus",
+        "com.yk.walk.plus", 
+    };
     local name = apps[currentIndex];
     if name == nil then
         currentIndex = 1;
@@ -123,35 +130,42 @@ function currentPackage()
     return name;
 end
 
-
-
-for i=1,9999 do
-    -- 检测应用是否打开
-    packageName = app.frontPackageName();
-    local currentPackageName = currentPackage()
-    if packageName ~= currentPackageName then
-        app.runApp(currentPackageName);
-        mSleep(3000);
-    end
-
-    page = getCurrentPage();
+function start()
+    mSleep(5000);
+    local page = getCurrentPage();
     if page == "运动" then
         -- 查找广告按钮并点击
-        result = findAdButtonAndClick();
+        local result = findAdButtonAndClick();
         if result == true then
             page = getCurrentPage();
             if page ~= "运动" then
                 mSleep(getRndNum(25000, 30000));
                 tapAction();
-                mSleep(3000);
             end
             currentIndex = currentIndex + 1;
         else
             currentIndex = currentIndex + 1;
         end
     else
-        toast("未知页面，返回"); 
+        toast("未知页面，返回");
         keycode.back();
         mSleep(2000)
+    end
+end
+
+while true do
+    -- 检测应用是否打开
+    local packageName = app.frontPackageName();
+    local currentPackageName = currentPackage()
+    if packageName ~= currentPackageName then
+        local flag = app.runApp(currentPackageName);
+        if  flag == true then
+            mSleep(5000);
+            start();
+        else
+            currentIndex = currentIndex + 1;
+        end
+    else
+        start();
     end
 end
